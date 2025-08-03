776cd8739eac05f82f473e55f84fce7ee9c3f3bb
Dakshatabhole <dakshata.bhole24@sakec.ac.in>
2025-08-03 18:55:52 +0530

    Delete conditionals.js

--- conditionals.js
+++
@@ -1,9 +0,0 @@
-let temperature = 25;
-
-if (temperature>30){
-    console.log("It's a lot day!");
-} else if (temperature > 20){
-    console.log("It's a pleasant day!");
-} else {
-    console.log("It's a bit chilly today.");
-}
