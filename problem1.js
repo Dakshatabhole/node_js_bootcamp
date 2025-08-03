8c6909cb13f2bb12347ca54076370728d4f8bcc3
Dakshatabhole <dakshata.bhole24@sakec.ac.in>
2025-08-03 18:57:11 +0530

    Delete problem1.js

--- problem1.js
+++
@@ -1,9 +0,0 @@
-//find the sum of all numbers below 1000 that are divisible by 3 or 5
-
-let sum=0;
-for(let i=0; i<1000; i++){
-    if(i%3==0 || i%5==0){
-     sum = sum+i;
-    }
-    }
-console.log(`The sum is: ${sum}`);
