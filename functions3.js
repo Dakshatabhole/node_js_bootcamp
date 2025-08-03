978af85dd0e6c55b9160f051af357df567c6c16a
Dakshatabhole <dakshata.bhole24@sakec.ac.in>
2025-08-03 11:13:36 +0530

    Delete functions3.js

--- functions3.js
+++
@@ -1,8 +0,0 @@
-const getsum = function(a,b) {return a+b;};
-
-const sum = getsum(15,20);
-console.log(`The sum of the numbers 15 & 20 is: ${sum}`);
-
-//another way to define a function
-const multiply = (x,y) => x*y;
-console.log(`The product of 5 & 6 is: ${multiply(5,6)}`);
